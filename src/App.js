d9193ff1d290e5218457f5adb8d5a55b50ab5297
Daniel Nowosad <daniel.nowosad89@gmail.com>
2022-07-15 12:06:10 +0200

    1.0

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Koszyk from "./components/Koszyk";
 import data from "./data";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  
-  const [cartItems, setCartItems] = useState([]);
   const { product } = data;
+  const [filteredList, setFilteredList] = useState(product);
+  const [selectedIp, setSelectedIp] = useState("");
+
+  const [cartItems, setCartItems] = useState([]);
+
+  const filterByIp = (filteredData) => {
+    if (!selectedIp) {
+      return filteredData;
+    }
+    const filteredTasmy = filteredData.filter(
+      (tasma) => tasma.ip.split(" ").indexOf(selectedIp) !== -1
+    );
+    return filteredTasmy;
+  };
+
+  const handleIpChange = (event) => {
+    setSelectedIp(event.target.value);
+  };
+
+  useEffect(() => {
+    var filteredData = filterByIp(product);
+    setFilteredList(filteredData);
+  }, [selectedIp]);
+
   //Dodawanie do tablicy (koszykowej)
   const onAdd = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
@@ -27,7 +49,12 @@ function App() {
     (
       <div className="App">
         <Header></Header>
-        <Main onAdd={onAdd} product={product}></Main>
+        <Main
+          onAdd={onAdd}
+          product={filteredList}
+          handleIpChange={handleIpChange}
+          selectedIp={selectedIp}
+        ></Main>
         <Koszyk onAdd={onAdd} cartItems={cartItems}></Koszyk>
       </div>
     )
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Product from './Product';
-
-
+import React from "react";
+import Product from "./Product";
 
 export default function Main(props) {
-    const {product, onAdd} = props;
-    return(
-    <div>Konfigurator Taśm Led
-        <div>
-            {product.map((index) =>
-                (
-                    <Product key={index.id} product={index} onAdd={onAdd}></Product>
-                )
-            )}
-
-
-        </div>
+  const { product, onAdd, handleIpChange, selectedIp } = props;
+  return (
+    <div>
+      Konfigurator Taśm Led
+      <div>Filtrowanie po IP:</div>
+      <select id="ip-input" value={selectedIp} onChange={handleIpChange}>
+        <option value=""></option>
+        <option value="ip44">IP44</option>
+        <option value="ip20">IP20</option>
+      </select>
+      <div>
+        {product.map((index) => (
+          <Product key={index.id} product={index} onAdd={onAdd}></Product>
+        ))}
+      </div>
     </div>
-
-    )
-};
+  );
+}
