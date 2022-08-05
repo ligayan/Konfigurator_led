b7d2b8642a119a481a132bdd0f634321df18d76c
Daniel Nowosad <daniel.nowosad89@gmail.com>
2022-08-05 12:16:08 +0200

    ?

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,37 +1,59 @@
 import React from "react";
 
 export default function Filters(props) {
-  const {inputQty,handleChangeQty, selectedIp, handleIpChange, selectedK, handleKChange, selectedV, handleVChange, handleReset} =props;
+  const {
+    inputQty,
+    handleChangeQty,
+    selectedIp,
+    handleIpChange,
+    selectedK,
+    handleKChange,
+    selectedV,
+    handleVChange,
+    handleReset,
+  } = props;
   return (
     <div className="filters">
       <div className="flex">
-        <div className="filter_header">Wybór stopnia ochrony:</div>
+        <div className="filter_header">
+          Wybór stopnia ochrony:
+          <span className="info">
+            ? <div className="opis">Informacje na temat IP</div>
+          </span>
+        </div>
         <select id="ip-input" value={selectedIp} onChange={handleIpChange}>
           <option value="">wybierz IP</option>
           <option value="20">IP20</option>
           <option value="65">IP65</option>
         </select>
-        <div className="opis">Informacje na temat IP</div>
       </div>
       <div className="flex">
-        <div className="filter_header">Wybór barwy:</div>
+        <div className="filter_header">
+          Wybór barwy:{" "}
+          <span className="info">
+            ? <div className="opis">Informacje na temat Barwy</div>
+          </span>
+        </div>
         <select id="ip-input" value={selectedK} onChange={handleKChange}>
           <option value="">wybierz barwę</option>
           <option value="3000">3000 K</option>
           <option value="4000">4000 K</option>
           <option value="0">Barwa Regulowana</option>
         </select>
-        <div className="opis">Informacje na temat BARWY</div>
       </div>
       <div className="flex">
-        <div className="filter_header">Wybór rodzaju zaislania:</div>
+        <div className="filter_header">
+          Wybór rodzaju zaislania:
+          <span className="info">
+            ? <div className="opis">Informacje na temat Zasilania</div>
+          </span>
+        </div>
         <select id="ip-input" value={selectedV} onChange={handleVChange}>
           <option value="">wybierz zasilanie</option>
           <option value="12">12V</option>
           <option value="24">24V</option>
           <option value="48">48V</option>
         </select>
-        <div className="opis">Informacje na temat Zasilania</div>
       </div>
       <div className="flex">
         <div className="filter_header">Długość:</div>
